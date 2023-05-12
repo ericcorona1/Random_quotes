e985467188fddb26103349ac7b3ae91a53d0106f
ericcorona1 <ericcorona3@gmail.com>
2023-05-11 20:53:28 -0700

    activated color change onClick and onLoad

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 const quoteSection = document.getElementById("quote");
 const authorSection = document.getElementById("author");
 const generateButton = document.getElementById("generate");
+const colorPalette = [
+  "#16a085",
+  "#27ae60",
+  "#2c3e50",
+  "#f39c12",
+  "#e74c3c",
+  "#9b59b6",
+  "#FB6964",
+  "#342224",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857",
+];
+const rootElement = document.querySelector(":root");
+
+//Change primary color var in css
+function changeColor() {
+  const randomColor = Math.floor(Math.random() * colorPalette.length);
+  const selectedColor = colorPalette[randomColor];
+  console.log(selectedColor);
+  rootElement.style.setProperty("--primary-color", selectedColor);
+}
 
 function generateQuote() {
-    fetch("https://api.quotable.io/quotes/random")
-        .then(res => res.json())
-        .then(data => {
-            console.log(data[0].content);
-            console.log(data[0].author);
-            const quoteText = data[0].content;
-            const quoteAuthor = data[0].author;
+  fetch("https://api.quotable.io/quotes/random")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data[0].content);
+      console.log(data[0].author);
+      const quoteText = data[0].content;
+      const quoteAuthor = data[0].author;
 
-            quoteSection.innerText = '"' + quoteText + '"';
-            authorSection.innerText = "- " + quoteAuthor;
-        })
+      quoteSection.innerText = '"' + quoteText + '"';
+      authorSection.innerText = "- " + quoteAuthor;
+    });
 }
 
 // Event listener
-generateButton.addEventListener('click', () => {
-    generateQuote();
-})
+generateButton.addEventListener("click", () => {
+  generateQuote();
+  changeColor();
+});
 
 generateQuote();
+changeColor();
